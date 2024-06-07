70fdb33418a3a5385555c1f7cbc8105818dba5e0
Nauka <berdinauryzbek@gmail.com>
2024-06-08 02:30:41 +0500

    run the screen

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from interface import Screen
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Main:
+    def __init__(self):
+        self.screen = Screen()
+
+    def run(self):
+        self.screen.run()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main = Main()
+    main.run()
