c1407f75a51017bb5d522a572ff8bd6eb40a02fb
Nauka <berdinauryzbek@gmail.com>
2024-06-08 02:30:26 +0500

    made the game screen

--- interface.py
+++ interface.py
@@ -1,2 +1,16 @@
-class screen():
-    pass
+from settings import *
+
+
+class Screen:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("BlackJack")
+
+    def run(self):
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+        pygame.quit()
