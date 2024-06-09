e178f46474aa5e01605be22614217a1cd277c662
Nauka <berdinauryzbek@gmail.com>
2024-06-09 22:19:24 +0500

    added icon for the game

--- interface.py
+++ interface.py
@@ -5,6 +5,8 @@ class Screen:
     def __init__(self):
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("BlackJack")
+        self.pygame_icon = pygame.image.load('img/blackjack.png')
+        pygame.display.set_icon(self.pygame_icon)
 
     def run(self):
         run = True
@@ -12,5 +14,7 @@ class Screen:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+
+            pygame.display.update()
 
         pygame.quit()
