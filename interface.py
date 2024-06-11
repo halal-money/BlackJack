87e2b40e61d34dbad4d166a286cb96f5555eb8ef
Nauka <berdinauryzbek@gmail.com>
2024-06-11 14:52:38 +0500

    displayed the game icon on the screen

--- interface.py
+++ interface.py
@@ -9,8 +9,8 @@ class Screen:
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("BlackJack")     # title of the game
 
-        self.pygame_icon = pygame.image.load('img/blackjack.png')
-        pygame.display.set_icon(self.pygame_icon)   # icon of the game
+        self.game_icon = pygame.image.load('img/blackjack.png')
+        pygame.display.set_icon(self.game_icon)   # icon of the game
 
         # bg of the game
         self.bg_image = pygame.transform.scale(pygame.image.load('img/desk.jpg'), (WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -22,13 +22,18 @@ class Screen:
         self.game_header = "BlackJack"   # header text
         self.header_color = (128, 0, 0)  # color of the header
         self.header_surface = self.font.render(self.game_header, True, self.header_color)
-        self.header_rect = self.header_surface.get_rect(center=(WINDOW_WIDTH / 2, 340))
+        self.header_rect = self.header_surface.get_rect(center=(WINDOW_WIDTH / 2, 215))
+
+        # center the game icon
+        self.icon_size = pygame.transform.scale(self.game_icon, (200, 150))
+        self.icon_rect = self.icon_size.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
     def run(self):
         run = True
         while run:
             self.screen.blit(self.bg_image, (0, 0))     # display bg img
-            self.screen.blit(self.header_surface, self.header_rect)
+            self.screen.blit(self.header_surface, self.header_rect)     # display header
+            self.screen.blit(self.icon_size, self.icon_rect)    # display game icon
             if self.start_button.run():     # display start btn img
                 print('Game Start')
             pygame.display.update()
