744df8ab8871ee0b9766c350558a487fc09aabc8
Nauka <berdinauryzbek@gmail.com>
2024-06-15 21:16:58 +0500

    modified the chips file by removing the class

--- chips.py
+++ chips.py
@@ -1,24 +1,22 @@
 import os
 
 from settings import *
-class Chips:
-    def __init__(self):
-        self.surface = pygame.surface.Surface((CHIPS_WIDTH, CHIPS_HEIGHT))
 
-        # Creating a dict with chip images and their value
-        self.chip_value = {}
-        for filename in os.listdir('img/Chips'):
-            if filename.startswith('25'):
-                image = pygame.image.load(f'img/Chips/{filename}')
-                self.chip_value['25'] = image
-            elif filename.startswith('500'):
-                image = pygame.image.load(f'img/Chips/{filename}')
-                self.chip_value['500'] = image
-            elif filename.startswith('50'):
-                image = pygame.image.load(f'img/Chips/{filename}')
-                self.chip_value['50'] = image
-            elif filename.startswith('100'):
-                image = pygame.image.load(f'img/Chips/{filename}')
-                self.chip_value['100'] = image
+chips_dict: dict = {}
+for filename in os.listdir('img/Chips'):
+    if filename.startswith('25'):
+        image = pygame.image.load(f'img/Chips/{filename}')
+        chips_dict['25'] = image
+    elif filename.startswith('500'):
+        image = pygame.image.load(f'img/Chips/{filename}')
+        chips_dict['500'] = image
+    elif filename.startswith('50'):
+        image = pygame.image.load(f'img/Chips/{filename}')
+        chips_dict['50'] = image
+    elif filename.startswith('100'):
+        image = pygame.image.load(f'img/Chips/{filename}')
+        chips_dict['100'] = image
+
+print(chips_dict)
 
 
