2a13d77d8f4a4ed1189a4a41101ffe3b8ca61f68
Nauka <berdinauryzbek@gmail.com>
2024-06-15 21:18:23 +0500

    used chips_dict to get imgs

--- display_chips.py
+++ display_chips.py
@@ -1,6 +1,7 @@
 import pygame.display
 from settings import *
 from button import Button
+from chips import chips_dict
 
 
 class Chips:
@@ -10,13 +11,13 @@ class Chips:
         self.display_surface = pygame.display.get_surface()
 
         # display chips
-        self.twenty_five = Button(button=pygame.image.load('img/chips/25-chip.png'), btn_size=(100, 100), btn_rect=(190, 650))
+        self.twenty_five = Button(button=chips_dict['25'], btn_size=(100, 100), btn_rect=(190, 650))
 
-        self.fifty = Button(button=pygame.image.load('img/chips/50-chip.png'), btn_size=(100, 100), btn_rect=(290, 650))
+        self.fifty = Button(button=chips_dict['50'], btn_size=(100, 100), btn_rect=(290, 650))
 
-        self.hundred = Button(button=pygame.image.load('img/chips/100.png'), btn_size=(100, 100), btn_rect=(395, 650))
+        self.hundred = Button(button=chips_dict['100'], btn_size=(100, 100), btn_rect=(395, 650))
 
-        self.five_hundred = Button(button=pygame.image.load('img/chips/500-chip.png'), btn_size=(100, 100), btn_rect=(500, 650))
+        self.five_hundred = Button(button=chips_dict['500'], btn_size=(100, 100), btn_rect=(500, 650))
 
     def run(self):
         # self.display_surface.blit(self.surface, self.rect)
